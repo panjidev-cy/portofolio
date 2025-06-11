c8542f65aedad23312e082ba8fbec40384f33f66
panjidev-cy <panjibhekti1@gmail.com>
2025-06-11 17:22:01 +0700

    Update HeroBranding.tsx
    masih error

--- src/components/fragments/hero/HeroBranding.tsx
+++ src/components/fragments/hero/HeroBranding.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const HeroBranding = () => (
   <span className="flex items-center gap-4">
     <div className="p-2 bg-yellow-300 rounded-full inline-block">
-      <Image src="/assets/v.svg" alt="Logo" width={25} height={25} />
+      <Image src="/assets/V.svg" alt="Logo" width={25} height={25} />
     </div>
     <span className="font-semibold text-h6">
       Vibloo.<span className="text-purple-600">creative</span>
