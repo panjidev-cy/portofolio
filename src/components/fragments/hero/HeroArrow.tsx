214855949d4a355c6f90f78bf8d4d7ea54f00fd9
panjidev-cy <panjibhekti1@gmail.com>
2025-06-11 20:56:40 +0700

    Update HeroArrow.tsx

--- src/components/fragments/hero/HeroArrow.tsx
+++ src/components/fragments/hero/HeroArrow.tsx
@@ -3,7 +3,7 @@
 import { TiArrowRightThick } from "react-icons/ti";
 import { motion } from "framer-motion";
 const HeroArrow = () => (
-  <motion.div initial={{ position: "absolute", left: 0, animationDelay: "2s" }} animate={{ left: "100%" }} transition={{ duration: 3, ease: "easeInOut" }} className="p-2 md:p-3 bg-yellow-300 rounded-full relative">
+  <motion.div initial={{ position: "absolute", left: 0, animationDelay: "2s", rotate: 0 }} animate={{ left: "90%", rotate: 360 }} transition={{ duration: 3, ease: "easeInOut" }} className="p-2 md:p-3 bg-yellow-300 rounded-full relative">
     <TiArrowRightThick className="w-6 h-6" />
   </motion.div>
 );
